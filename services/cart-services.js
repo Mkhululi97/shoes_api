686a4140127c6d850416c10b22eeb71ec3d02e1a
mkhululi <mkhululimtshali@gmail.com>
2023-11-08 08:19:44 +0200

    updating total on cart

--- services/cart-services.js
+++ services/cart-services.js
@@ -71,6 +71,8 @@ export default function shoeFunctions(db) {
     return error;
   }
   async function getCart(email) {
+    let totalShoe;
+    let totalCart = 0;
     let cartid = await db.any(
       `SELECT cart_id
        FROM shoe_api_schema.orders AS orders
@@ -89,18 +91,12 @@ export default function shoeFunctions(db) {
       WHERE email=$1`,
       [email]
     );
-    let total = await db.any(
-      `SELECT SUM (new_price)
-      FROM shoe_api_schema.orders AS orders
-      INNER JOIN shoe_api_schema.shoe_details AS shoe_det ON shoe_det.id=orders.shoe_id
-      INNER JOIN shoe_api_schema.cart AS cart ON cart.id=orders.cart_id
-      INNER JOIN shoe_api_schema.users AS users ON users.id=cart.user_id
-      WHERE email=$1`,
-      [email]
-    );
+    shoesArr.forEach((shoe) => {
+      totalShoe = shoe.qty * shoe.new_price;
+      totalCart += totalShoe;
+    });
     cartid = cartid[0].cart_id;
-    total = total[0].sum;
-    return { cartid, email, shoesArr, total };
+    return { cartid, email, shoesArr, totalCart };
   }
   return {
     add,
